dfa4389dc5c685de23e00030173b53084b5b1491
Prakalp Pandey <55628058+prakalppandey1997@users.noreply.github.com>
2019-09-21 22:06:49 +0530

    CRUD APPLICATION
    Used to make change in database by creating,reading , updating and deleting keys

---
+++ logout.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+session_destroy();
+header('Location: index.php');
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>PRAKALP PANDEY</title>
+</head>
+<body>
+
+</body>
+</html>
